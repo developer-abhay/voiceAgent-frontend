61fcdb491ed806ab70e384fd15c930e7def01149
Abhay <abhaysharma.developer@gmail.com>
2024-11-13 00:10:22 +0530

    Not found page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import AuthPage from "./pages/AuthPage"
 import Dashboard from "./pages/Dashboard"
 import CreateAgent from "./pages/CreateAgent"
+import NotFound from "./pages/NotFound"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/login' element={<AuthPage />} />
-        <Route path='/dashboard/*' element={<Dashboard />} />
         <Route path='/create-agent' element={<CreateAgent />} />
+        <Route path='/dashboard/*' element={<Dashboard />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
   )
